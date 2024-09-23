7099047e3940539a91ba158c5597ae4cf7c8ba97
Maria Eduarda <silvamariaeduarda92@gmail.com>
2024-09-23 20:47:15 -0300

    fix: fixed complaince table

--- database/migrations/2024_08_07_215850_add_soft_deletes_to_complainces_table.php
+++ database/migrations/2024_08_07_215850_add_soft_deletes_to_complainces_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('complaince', function (Blueprint $table) {
+        Schema::table('complainces', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('complaints', function (Blueprint $table) {
+        Schema::table('complainces', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }

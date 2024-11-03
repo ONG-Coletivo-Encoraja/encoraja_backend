6f6bc6dc844977ace227efcfe6ee209e5df3cee7
Maria Eduarda da Silva <silvamariaeduarda92@gmail.com>
2024-11-03 20:42:02 -0300

    changed token expire time

--- config/auth.php
+++ config/auth.php
@@ -105,7 +105,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
+            'expire' => 480,
             'throttle' => 60,
         ],
     ],
